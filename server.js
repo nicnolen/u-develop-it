94fa321f650bbb64265d814fc49af7b4fea6e009
Nicholas Nolen <nicnolen@ymail.com>
2022-01-24 15:49:07 -0600

    added mysql2

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 // Import npm packages
 // Import Express.js
 const express = require('express');
+// Import mysql2
+const mysql = require('mysql2');
 
 // Add the PORT designation
 const PORT = process.env.PORT || 3001;
@@ -13,6 +15,23 @@ app.use(express.urlencoded({ extended: false }));
 // Recognize incoming Request Object as a JSON Object
 app.use(express.json());
 
+// Connect to the mysql2 database
+const db = mysql.createConnection(
+  {
+    host: '',
+    // Your MySQL username
+    user: 'root',
+    // MySQL password
+    password: '',
+    database: 'election',
+  },
+  console.log('Connected to the election database.')
+);
+
+// return all the data in the candidates table
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+  console.log(rows);
+});
 // Add a route to handle user requests that aren't supported by the app
 app.use((req, res) => {
   res.status(404).end();
