12503520858717f3fc1eaf71b32f867312a343e4
Nicholas Nolen <nicnolen@ymail.com>
2022-01-25 08:05:26 -0600

    GET request to delete a candidate

--- server.js
+++ server.js
@@ -62,13 +62,27 @@ app.get('/api/candidate/:id', (req, res) => {
   });
 });
 
-// // DELETE a candidate
-// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
-//   if (err) {
-//     console.log(err);
-//   }
-//   console.log(result);
-// });
+// DELETE a candidate
+app.delete('/api/candidate/:id', (req, res) => {
+  const sql = `DELETE FROM candidates WHERE id = ?`;
+  const params = [req.params.id];
+
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.statusMessage(400).json({ error: res.message });
+    } else if (!result.affectedRows) {
+      res.json({
+        message: 'Candidate not found',
+      });
+    } else {
+      res.json({
+        message: 'successfully deleted',
+        changes: result.affectedRows,
+        id: req.params.id,
+      });
+    }
+  });
+});
 
 // // CREATE a candidate
 // const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
