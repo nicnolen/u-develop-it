c8a177bb9fa6e85138bafd25ddbd74fbdddbb96d
Nicholas Nolen <nicnolen@ymail.com>
2022-01-26 13:46:27 -0600

    made a parties table and populated

--- db/schema.sql
+++ db/schema.sql
@@ -3,6 +3,13 @@ NOT NULL means the column cant be blank
 AUTOINCREMENT (automatically increments for each row starting from 1) 
 PRIMARY KEY(gives unique values) */
 
+/* Create a parties table */
+CREATE TABLE parties (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  description TEXT
+);
+
 /* Create a candidates table with 4 columns.*/
 CREATE TABLE candidates (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,10 @@
+/* Populate the parties table */
+INSERT INTO parties (name, description)
+VALUES
+  ('JS Juggernauts', 'The JS Juggernauts eat, breathe, and sleep JavaScript. They can build everything you could ever want in JS, including a new kitchen sink.'),
+  ('Heroes of HTML', 'Want to see a mock-up turn into an actual webpage in a matter of minutes? Well, the Heroes of HTML can get it done in a matter of seconds.'),
+  ('Git Gurus', 'Need to resolve a merge conflict? The Git Gurus have your back. Nobody knows Git like these folks do.');
+  
 /* POPULATE THE candidates TABLE WITH DATA */
 INSERT INTO candidates (first_name, last_name, industry_connected)
 values('Ronald', 'Firbank', 1),
